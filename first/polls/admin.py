825b171ced1b95cb9b514784929fe410b8d83b00
n2o <cmeter@googlemail.com>
2013-11-20 17:39:21 +0100

    Going forward in the tutorial...

--- first/polls/admin.py
+++ first/polls/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from polls.models import Choice
+from polls.models import Choice, Poll
 
-admin.site.register(Choice)
-
-from polls.models import Poll
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
 
 class PollAdmin(admin.ModelAdmin):
 	# Change order of fields:
@@ -14,4 +14,7 @@ class PollAdmin(admin.ModelAdmin):
 		('Bombe', 				{'fields': ['question']}),
 		('Date Information',	{'fields': ['pub_date'], 'classes': ['collapse']}),
 	]
+	inlines = [ChoiceInline]
+
 admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
