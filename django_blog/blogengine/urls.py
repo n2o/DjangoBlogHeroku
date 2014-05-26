506eee30ced84ded8d2a249d83987da5ac8a3e76
n2o <cmeter@googlemail.com>
2014-05-26 23:24:31 +0200

    Implemented post pages

--- django_blog/blogengine/urls.py
+++ django_blog/blogengine/urls.py
@@ -8,4 +8,9 @@ urlpatterns = patterns('',
         model=Post,
         paginate_by=5,
         )),
+
+    # Individual posts
+    url(r'^(?P<pub_date__year>\d{4})/(?P<pub_date__month>\d{1,2})/(?P<slug>[a-zA-Z0-9-]+)/?$', DetailView.as_view(
+        model=Post,
+        )),
 )
