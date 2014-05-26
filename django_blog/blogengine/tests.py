74fed31363f22c97ffa0ceebd25391a3ac87eca4
n2o <cmeter@googlemail.com>
2014-05-26 21:40:51 +0200

    Refactoring tests

--- django_blog/blogengine/tests.py
+++ django_blog/blogengine/tests.py
@@ -27,18 +27,36 @@ class PostTest(TestCase):
 
 
 class AdminTest(LiveServerTestCase):
-    def test_login(self):
-        c = Client()
+    fixtures = ['users.json']
 
+    def setUp(self):
+        self.client = Client()
+
+    def test_login(self):
         # Get login page
-        response = c.get('/admin/')
+        response = self.client.get('/admin/')
 
         # Check response code
         self.assertEquals(response.status_code, 200)
 
         # Log the user in
-        c.login(username="bobsmith", password="password")
+        self.client.login(username='bobsmith', password="password")
 
         # Check response code
-        response = c.get('/admin/')
+        response = self.client.get('/admin/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_logout(self):
+        # Log in
+        self.client.login(username='bobsmith', password="password")
+
+        # Check response code
+        response = self.client.get('/admin/')
+        self.assertEquals(response.status_code, 200)
+
+        # Log out
+        self.client.logout()
+
+        # Check response code
+        response = self.client.get('/admin/')
         self.assertEquals(response.status_code, 200)
